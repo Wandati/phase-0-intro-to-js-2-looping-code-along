ed965b5b60d4b731c6777831e604b524339a0724
Wandati <Petersimon801@gmail.com>
2023-06-16 13:46:52 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(array,event){
+    let NewArray = [];
+    for (let i = 0; i < array.length; i++){
+        NewArray.push(`Thank you , ${array[i]} for the Wonderful ${event} gift!`);
+    }
+    return NewArray
+}
+writeCards(["Guadalupe", "Ollie", "Aki"],"suprise")
+
+function countDown(num){
+
+    while (num >= 0 ){
+        console.log(num);
+        num --; 
+    }
+
+}
+countDown(10)
+countDown(4)
